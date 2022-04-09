f6096a88e2beeecb1e9862e2c1d02ebd27209f72
Jack Cho <jack06215@gmail.com>
2022-04-09 11:05:39 +0800

    singleton with express

--- singleton-pattern/src/index.ts
+++ singleton-pattern/src/index.ts
@@ -1,7 +1,13 @@
+import mongoose from "mongoose";
+import winston from "winston";
+import path from "path";
+import express from "express";
+import http from "http";
+
 export class Singleton {
   static instance: Singleton;
   id: number;
-  
+
   constructor(id: number) {
     this.id = id;
 
@@ -20,3 +26,57 @@ const OBJECT2 = new Singleton(2); // setting its id property to 2
 console.log(OBJECT1 === OBJECT2); // = true
 console.log(OBJECT1.id); // returns 1
 console.log(OBJECT2.id); // returns 1
+
+/**
+ * A Mongoose Example
+ */
+class ServerGlobal {
+  private static _instance: ServerGlobal;
+  private readonly _logger: winston.Logger;
+
+  private constructor() {
+    this._logger = winston.createLogger({
+      level: "info",
+      format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.json()
+      ),
+      transports: [
+        new winston.transports.Console(),
+        new winston.transports.File({
+          filename: path.join(__dirname, "../logs.log"),
+          level: "info",
+        }),
+      ],
+    });
+
+    mongoose
+      .connect(process.env.DB_ENDPOINT!)
+      .then(() =>
+        this._logger.info("MongoDB connection established successfully")
+      )
+      .catch((e: mongoose.MongooseError) =>
+        this._logger.error(`MongoDB connection failed with error: ${e}`)
+      );
+  }
+
+  static getInstance() {
+    if (this._instance) {
+      return this._instance;
+    }
+
+    this._instance = new ServerGlobal();
+    return this._instance;
+  }
+
+  public get logger() {
+    return this._logger;
+  }
+}
+
+const app = express();
+app.set("port", 9999);
+
+const server = http.createServer(app);
+const instance = ServerGlobal.getInstance();
+instance.logger.info(`Server is running on port ${process.env.PORT}`);
